1f9d739006bf49958b4578a8df314f762f4eb00b
Francisco Flores <ffloresm@inf.udec.cl>
2019-06-15 15:15:34 -0400

    first commit

---
+++ krustal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void lee_grafo(){
+	
+}
+
+int main(int argc, char const *argv[])
+{
+	
+	int a,b,c;
+	while (cin >> a >> b >> c){
+		cout << a<< " " << b <<" " << c << endl;	
+	}
+	
+	return 0;
+}
