4bb0f7d9547d7c7b2faa7c25ba97877f40c5dd15
Francisco Flores <ffloresm@inf.udec.cl>
2019-06-18 14:54:10 -0400

    leer grafo

---
+++ krustal.py
@@ -0,0 +1,16 @@
+import grafo
+import sys
+
+def leer_grafo(file):
+	g = set()
+	with open(file, 'r') as file:
+		content = file.read().splitlines()
+	for i in content:
+		g.add(tuple(int(x) for x in i.split()))
+	return g
+#print(sys.argv)
+archivo = sys.argv[1]
+g = leer_grafo(archivo)
+grafo = grafo.Grafo(g)
+print(grafo.Vertices())
+print(grafo.Aristas())
